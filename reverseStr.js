253201826a0ebf78031492b5ceede0e5e69d67c3
zeenat <zeenu.23@gmail.com>
2024-07-01 17:04:05 +0530

    shortcut way for `reverseString`

--- reverseStr.js
+++ reverseStr.js
@@ -1,7 +1,7 @@
 // Write a function that returns the reverse of a string.
 
+// using for loop
 function reverseStr(str) {
-
     let reversed = "";
 
     for (let i = str.length - 1; i >= 0; i--) {
@@ -11,3 +11,13 @@ function reverseStr(str) {
 }
 
 console.log(reverseStr('Interview, Happy'))
+
+// shortcut way
+
+function reverseString(str) {
+    return str.split('').reverse().join('');    
+}
+
+console.log(reverseString('hello'))
+
+
