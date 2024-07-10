8a7c0ee3e1b7ab17c083ef1a19d11ecdcb3e1230
zeenat <zeenu.23@gmail.com>
2024-07-10 10:43:51 +0530

    find sum of all elements

---
+++ findSum.js
@@ -0,0 +1,19 @@
+// Write a function that returns the sum of all elements.
+
+function sumOfElements(arr) {
+    return arr.reduce((a,b) => a+b, 0)
+}
+
+console.log(sumOfElements([1, 2, 4, 5, 8]))
+
+// another way
+
+function findSum(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];        
+    }
+    return sum
+}
+
+console.log(findSum([3, 2, 4, 5, 8]))
